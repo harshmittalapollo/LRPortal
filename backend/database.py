cf89a788d0392ee287175439b418fbd45c409119
Harsh Mittal <harsh.mittal@apollosupplychain.com>
2026-07-01 16:38:17 +0530

    Fix MySQL driver issue

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,17 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise Exception("DATABASE_URL environment variable missing")
+
+# Force SQLAlchemy to use pymysql
+DATABASE_URL = DATABASE_URL.replace(
+    "mysql://",
+    "mysql+pymysql://"
+)
 
 engine = create_engine(
     DATABASE_URL,
