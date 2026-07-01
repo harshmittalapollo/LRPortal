5bd69295d8fb4867d1d99a926ece532f108387c6
Harsh Mittal <harsh.mittal@apollosupplychain.com>
2026-07-01 15:59:19 +0530

    Fixed env variable setup

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable missing")
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
