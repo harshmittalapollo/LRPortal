b07580ff1258b37b80bafe71c76f7394634b1afa
Harsh Mittal <harsh.mittal@apollosupplychain.com>
2026-07-01 15:51:57 +0530

    Fixed database connection

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
@@ -7,3 +8,11 @@ engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
